8c9161c36699ebba5528601a12a5079696bc2bfe
admin <admin@admins-MacBook-Pro.local>
2024-10-26 17:48:39 -0500

    Activate GPIOA clock.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,19 +19,18 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-#define PIEZO_CHARGE_PIN 9
-#define PIEZO_DISCHARGE_PIN 10
-#define OUTPUT_MODE 1
+#define PIEZO_CHARGE_PIN 9U
+#define PIEZO_DISCHARGE_PIN 10U
+#define OUTPUT_MODE 1U
 #define HIGH_SPEED_OUTPUT 0x03
-#define PIEZO_CHARGE_PIN_HIGH (1 << PIEZO_CHARGE_PIN)
-#define PIEZO_DISCHARGE_PIN_HIGH (1 << PIEZO_DISCHARGE_PIN)
+#define PIEZO_CHARGE_PIN_HIGH (1U << PIEZO_CHARGE_PIN)
+#define PIEZO_DISCHARGE_PIN_HIGH (1U << PIEZO_DISCHARGE_PIN)
 
 
 
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
 
 static void initGPIO(void);
 static void chargePiezo(void);
@@ -49,24 +48,71 @@ int main(void)
   /* Configure the system clock */
   SystemClock_Config();
 
-
-
-  /* Initialize all configured peripherals */
-//  MX_GPIO_Init();
   initGPIO();
 
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
   {
     chargePiezo();
     HAL_Delay(1000);
     dischargePiezo();
+    HAL_Delay(1000);
+
 
   }
-  /* USER CODE END 3 */
 }
+
+
+
+static void initGPIO(void)
+{
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	GPIOA->MODER |= (OUTPUT_MODE << (PIEZO_CHARGE_PIN * 2));
+	GPIOA->MODER |= (OUTPUT_MODE << (PIEZO_DISCHARGE_PIN * 2));
+
+
+	GPIOA->OSPEEDR |= (HIGH_SPEED_OUTPUT << (PIEZO_CHARGE_PIN * 2));
+	GPIOA->OSPEEDR |= (HIGH_SPEED_OUTPUT << (PIEZO_DISCHARGE_PIN * 2));
+
+	return;
+}
+
+static void chargePiezo(void)
+{
+	//Before activating, make sure that the discharge transistor is turned off!
+	if(GPIOA->ODR & PIEZO_DISCHARGE_PIN_HIGH)
+	{
+		GPIOA->BRR = PIEZO_DISCHARGE_PIN_HIGH; //clear discharge pin bit
+	}
+
+	//Activate the transistor to begin charging the piezo.
+	GPIOA->BSRR = PIEZO_CHARGE_PIN_HIGH;
+}
+
+static void dischargePiezo(void)
+{
+	//Before activating, make sure that the charge transistor is turned off!
+	if(GPIOA->ODR & PIEZO_CHARGE_PIN_HIGH)
+	{
+		GPIOA->BRR = PIEZO_CHARGE_PIN_HIGH; // clear charge pin bit.
+	}
+
+
+	//Active the transistor to drain the piezo.
+	GPIOA->BSRR = PIEZO_DISCHARGE_PIN_HIGH;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 /**
   * @brief System Clock Configuration
@@ -105,84 +151,7 @@ void SystemClock_Config(void)
 }
 
 
-static void initGPIO(void)
-{
-	GPIOA->MODER = (OUTPUT_MODE << (PIEZO_CHARGE_PIN * 2));
-	GPIOA->MODER = (OUTPUT_MODE << (PIEZO_DISCHARGE_PIN * 2));
 
-	GPIOA->OSPEEDR = (HIGH_SPEED_OUTPUT << (PIEZO_CHARGE_PIN * 2));
-	GPIOA->OSPEEDR = (HIGH_SPEED_OUTPUT << (PIEZO_DISCHARGE_PIN * 2));
-
-	return;
-}
-
-static void chargePiezo(void)
-{
-	//Before activating, make sure that the discharge transistor is turned off!
-	if(GPIOA->ODR & PIEZO_DISCHARGE_PIN_HIGH)
-	{
-		GPIOA->ODR &= ~PIEZO_DISCHARGE_PIN_HIGH; //clear discharge pin bit
-	}
-
-	//Activate the transistor to begin charging the piezo.
-	GPIOA->ODR |= PIEZO_CHARGE_PIN_HIGH;
-}
-
-static void dischargePiezo(void)
-{
-	//Before activating, make sure that the charge transistor is turned off!
-	if(GPIOA->ODR & PIEZO_CHARGE_PIN_HIGH)
-	{
-		GPIOA->ODR &= ~PIEZO_CHARGE_PIN_HIGH; // clear charge pin bit.
-	}
-
-
-	//Active the transistor to drain the piezo.
-	GPIOA->ODR |= PIEZO_DISCHARGE_PIN_HIGH;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : PA0 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
 
 /**
   * @brief  This function is executed in case of error occurrence.
